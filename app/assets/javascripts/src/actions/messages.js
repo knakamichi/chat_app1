cd084ecf68bb475e43578e47c95bd194d81d6e65
kokezeror <kokinakamichi0617@g.ecc.u-tokyo.ac.jp>
2017-11-01 12:46:57 +0900

    Build Flux

---
+++ app/assets/javascripts/src/actions/messages.js
@@ -0,0 +1,21 @@
+// actions/messages.js
+import Dispatcher from '../dispatcher'
+import {ActionTypes} from '../constants/app'
+
+export default {
+  changeOpenChat(newUserID) {
+    Dispatcher.handleViewAction({
+      type: ActionTypes.UPDATE_OPEN_CHAT_ID, 
+      userID: newUserID,
+    })
+  },
+
+  sendMessage(userID, message) {
+    Dispatcher.handleViewAction({
+      type: ActionTypes.SEND_MESSAGE,
+      userID: userID,
+      message: message,
+      timestamp: +new Date(),
+    })
+  },
+}
