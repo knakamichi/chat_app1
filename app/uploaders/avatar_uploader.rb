98169cf4e0776e51be21438cf8f89d4bfa0e4b50
Koki Nakamichi <kokinakamichi0617@g.ecc.u-tokyo.ac.jp>
2018-01-11 03:37:13 +0900

    Re-edit css (friendRequests, header)

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -32,7 +32,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [100, 100]
+    process :resize_to_fit => [80, 80]
   end
 
   version :small do
